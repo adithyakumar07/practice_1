ac210d636983937418576016560fea0b6c9d1f97
AdithyaKumar <kumaradithya537@gmail.com>
2025-05-01 19:13:39 +0530

    arthematic_operator

---
+++ arthematic_operator_7.py
@@ -0,0 +1,8 @@
+a=7
+b=6
+c=a+b
+j=a-b
+k=a*b
+print("value of C :",c)
+print("value of J :",j)
+print("value of K :",k)
---
+++ arthematic_operator_8.py
@@ -0,0 +1,6 @@
+a=7
+b=8
+c=a*b
+j=a**b
+print("value of C :",c)
+print("value of J :",j)
